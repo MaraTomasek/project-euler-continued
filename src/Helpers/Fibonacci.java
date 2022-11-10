94f2026584d1ee402a86f48ce56edc7846f19904
Mara Tomasek <mara.tomasek@adesso.de>
2022-11-10 15:57:33 +0100

    5 - created helper package

---
+++ src/Helpers/Fibonacci.java
@@ -0,0 +1,24 @@
+package Helpers;
+
+public class Fibonacci {
+
+    public static long fibonacci(long termTarget) {
+        if (termTarget <= 0) return 0;
+        if (termTarget == 1) return 1;
+
+        long term1 = 0;
+        long term2 = 1;
+        long termValue = 1;
+        for (int termIndex = 2; termIndex <= termTarget; termIndex++) {
+            termValue = term1 + term2;
+            term1 = term2;
+            term2 = termValue;
+
+            if (term2 < term1) {
+                throw new RuntimeException(String.format("Overflow at Term %d - Last correct value is %d: %d", termIndex, termIndex - 1, term1));
+            }
+        }
+
+        return termValue;
+    }
+}
